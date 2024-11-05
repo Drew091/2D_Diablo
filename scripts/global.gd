b144d19afbcbc3003138e5bfbdc4208d305e5a81
Drew091 <hoppal.andras91@gmail.com>
2024-11-05 19:58:18 +0100

    Create the base game
    Create the base game from a You Tube tutorial, add death animation and respawn for "R" button and add my first pixel art as character with idle and walk animation

---
+++ scripts/global.gd
@@ -0,0 +1,24 @@
+extends Node
+
+var player_alive = true
+var player_current_attack = false
+
+var current_scene = "world" #world cliff_side
+var transition_scene = false
+
+var player_exit_cliffside_posx = 199
+var player_exit_cliffside_posy = 29
+var player_start_posx = 137
+var player_start_posy = 128
+
+var game_first_loadin = true
+
+func finish_changescenes():
+	print(current_scene)
+	print(transition_scene)
+	if transition_scene == true:
+		transition_scene = false
+		if current_scene == "world":
+			current_scene = "cliff_side"
+		else: 
+			current_scene = "world"
